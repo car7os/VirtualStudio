b67bdfc131aae27c489f9ef617b9bf19c2577671
root <carlosbezerravilela@gmail.com>
2018-12-01 00:44:27 -0200

    Projeto Java - Criação da Classe AudioMedia (contem bug)

--- VirtualStudio/src/principal/Main.java
+++ VirtualStudio/src/principal/Main.java
@@ -13,7 +13,7 @@ import java.io.IOException;
 import java.util.Properties;
 import org.apache.commons.lang.SystemUtils;
 
-import funcionalidades.executar.*;
+import funcionalidades.midia.*;
 
 //+------------------------------------------+
 //| Classe Principal                         |
@@ -21,8 +21,21 @@ import funcionalidades.executar.*;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, InterruptedException {
 		System.out.println(">> Inicializando");
+		
+		AudioPlayer p1,p2;
+		
+		p1= new AudioPlayer("C:\\Users\\root\\Desktop\\Unidade de USB\\A (2).mp3");
+		p1.play();
+		Thread.currentThread();
+		Thread.sleep(7000);
+		p1.stop();
+		p2= new AudioPlayer("C:\\Users\\root\\Desktop\\Unidade de USB\\A (1).mp3");
+		p2.play();
+	
+		
+		
 	}
 
 }
